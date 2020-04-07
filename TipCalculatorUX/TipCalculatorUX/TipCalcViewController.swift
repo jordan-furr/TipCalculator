34919bc6ccc6e3b11045e8d1e268fbd8d37bf67f
Jordan Furr <you@example.com>
2020-04-07 17:37:13 -0400

    Math and buttons

--- TipCalculatorUX/TipCalculatorUX/TipCalcViewController.swift
+++ TipCalculatorUX/TipCalculatorUX/TipCalcViewController.swift
@@ -13,30 +13,47 @@ class TipCalcViewController: UIViewController {
     
 
     var tip: Double = 0.0
+    var percent = 0.20
+
+    @IBOutlet weak var percentLabel: UILabel!
     
     @IBOutlet weak var sliderBar: UISlider!
+    
+    
     @IBAction func fifteenTapped(_ sender: Any) {
     }
     @IBAction func eighteentapped(_ sender: Any) {
     }
     
+    @IBOutlet weak var percentbutton: UIButton!
     @IBOutlet weak var mealCostLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var finalPriceLabel: UILabel!
     
-    
+    @IBOutlet weak var funStackView: UIStackView!
     
     var total: Double = 0.0
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+       // view.backgroundColor = UIColor(named: "20percent")
         updateViews()
-        mealCostLabel.text = "Meal: $ \(total)"
     }
     override func viewDidLoad() {
+
         super.viewDidLoad()
     }
     
     func updateViews(){
-        tipLabel.text = "Tip: $\(total * 0.2)"
+        calculateTip()
+        mealCostLabel.text = "$ \(total)"
+        tipLabel.text = "$ "
+        tipLabel.text = tipLabel.text! + String(format: "%.2f", tip)
+        finalPriceLabel.text = "$ "
+        finalPriceLabel.text = finalPriceLabel.text! + String(format: "%.2f", (tip + total))
+    }
+    
+    func calculateTip() {
+        tip = total * percent
     }
 }
